68002253d9865cf1ceceb4feca61a60c47165e2e
masakanobuhiro <masakamasaka0315@gmail.com>
2021-04-22 15:27:30 +0900

    完成

--- main.go
+++ main.go
@@ -2,45 +2,12 @@ package main
 
 import (
   "fmt"
-  // "log"
-  // "todo_app/config"
   "todo_app/app/models"
+  "todo_app/app/controllers"
 )
 
 func main() {
-  // fmt.Println(config.Config.Port)
-  // fmt.Println(config.Config.SQLDriver)
-  // fmt.Println(config.Config.DbName)
-  // fmt.Println(config.Config.LogFile)
+  fmt.Println(models.Db)
 
-  // log.Println("test")
-
-  // fmt.Println(models.Db)
-
-  // // ユーザ作成
-  // u := &models.User{}
-  // u.Name = "test"
-  // u.Email = "test@example.com"
-  // u.PassWord = "testtest"
-  // fmt.Println(u)
-  // u.CreateUser()
-
-  // u, _ := models.GetUser(1)
-  // fmt.Println(u)
-
-  // u.Name ="Test2"
-  // u.Email ="Test2@example.com"
-  // u.UpdateUser()
-  // u, _ = models.GetUser(1)
-  // fmt.Println(u)
-
-  // u.DeleteUser()
-  // u, _ = models.GetUser(1)
-  // fmt.Println(u)
-
-  // user, _ := models.GetUser(2)
-  // user.CreateTodo("First Todo")
-
-  t, _ := models.GetTodo(1)
-  fmt.Println(t)
+  controllers.StartMainServer()
 }
